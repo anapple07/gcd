5e8cb5ea68e2b0bc74e909a2b5064f2504866bca
Anna <anapple07@users.noreply.github.com>
2017-04-29 13:22:13 +0900

    fix zero

--- gcd.sh
+++ gcd.sh
@@ -6,7 +6,7 @@ read m
 echo -n "please input second number:"
 read n
 
-IsCheckNumber () {
+IsNumber () {
 	x=$1
 	y=$2
 	expr $x + 1 > /dev/null 2>&1
@@ -20,23 +20,19 @@ IsCheckNumber () {
   		echo "please input number"
 		exit
 	fi
-
-	if [ $2 -gt $1 ]; then
-		x=$2
-		y=$1	
-	fi
-
-	if [ $y -eq 0  ]; then
-		echo "please don't input 0"
-		exit
-	fi
 }
 
-IsCheckNumber $m $n
 x=$m
 y=$n
-r=1
 
+IsNumber $x $y
+
+if [ $x  -gt $y ] && [ $y  -eq 0 ]; then
+	x=$n
+	y=$m
+fi
+
+r=1
 until [ $r == 0 ]
 do
    let "r =$x % $y"
