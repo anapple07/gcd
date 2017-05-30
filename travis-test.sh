c6df53792ef51bdd3b294427950d0c90aa6dc0e3
Anna <anapple07@users.noreply.github.com>
2017-05-30 19:00:40 +0900

    add travis-test.sh

--- travis-test.sh
+++ travis-test.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
-#OK test
-bash ./gcd.sh;
+tmp=/tmp/$$
 
-expect -c "
-set timeout 20
-expect \"\please input first number:";
-send \"12\r\";
-interact
-"
+ERROR_EXIT () {
+    echo "$1" >&2
+    rm -f $tmp-*
+    exit 1
+}
+#########
+# TEST1 #
+#########
+echo aaa bbb NG > $tmp-ans
+./gcd.sh aaa bbb > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
 
+#########
+# TEST2 #
+#########
+echo "same2.sh requires 2 string inputs" > $tmp-ans
+./gcd.sh 2> $tmp-err && ERROR_EXIT "TEST2-1"
+diff $tmp-ans $tmp-err || ERROR_EXIT "TEST2-2"
+
+echo OK
+rm -f $tmp-*
+exit 0
