e5382d22508af91ef352dbbe5c6342df77d7c018
Anna <anapple07@users.noreply.github.com>
2017-06-10 15:01:43 +0900

    add space

--- travis-test.sh
+++ travis-test.sh
@@ -15,17 +15,17 @@ diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
 
 # TEST2
 echo please input number > $tmp-ans
-./gcd.sh 2> $tmp-err || ERROR_EXIT "TEST2-1"
-diff $tmp-ans $tmp-err && ERROR_EXIT "TEST2-2"
+./gcd.sh 2 > $tmp-err || ERROR_EXIT "TEST2-1"
+diff $tmp-ans $tmp-err || ERROR_EXIT "TEST2-2"
 
 # TEST3
 echo dont input 0 and 0 > $tmp-ng
-./gcd.sh 0 0> $tmp-zero || ERROR_EXIT "TEST3-1"
-diff $tmp-ng $tmp-zero && ERROR_EXIT "TEST3-2"
+./gcd.sh 0 0 > $tmp-zero || ERROR_EXIT "TEST3-1"
+diff $tmp-ng $tmp-zero || ERROR_EXIT "TEST3-2"
 
 # TEST4
 echo 12 > $tmp-ok
-./gcd.sh 12 24> $tmp-input || ERROR_EXIT "TEST4-1"
+./gcd.sh 12 24 > $tmp-input || ERROR_EXIT "TEST4-1"
 diff $tmp-ok $tmp-input || ERROR_EXIT "TEST4-2"
 
 
